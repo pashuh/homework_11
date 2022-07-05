e8c6bd153d5061939f4f3a8aba1ac1658d4b50a5
pashuh <pashuh08@yandex.ru>
2022-07-05 15:35:29 +0300

    update TestBase

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -20,6 +20,7 @@ public class TestBase {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
         Configuration.browserPosition = "00x00";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 
     @AfterEach
